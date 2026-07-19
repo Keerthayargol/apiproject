5edd74d386f8b9cc47a473e43e7682a8d1cbab30
KEERTHAN <151749290+Keerthayargol@users.noreply.github.com>
2026-07-19 11:41:23 +0000

    Improved UI layout and styling

--- app.js
+++ app.js
@@ -1,21 +1,39 @@
-let btn1=document.querySelector("button");
+let btn1=document.querySelector("#btn1");
 btn1.addEventListener("click",async()=>{
-     let data=await getfacts();
-     let para=document.querySelector("p");
-     para.innerHTML=data;
+    let data=await getfacts();
+    let para=document.querySelector("p");
+    para.innerText=data;
 
 })
-
+let btn2=document.querySelector("#dog");
+btn2.addEventListener("click" ,async()=>{
+    let image=await dogfacts();
+    console.log(image);
+    let img=document.querySelector("img");
+    img.setAttribute("src",image);
+})
 
 let url="https://catfact.ninja/fact";
-async function getfacts(){
+let url2="https://dog.ceo/api/breeds/image/random";
+async function getfacts() {
     try{
     let res=await axios.get(url);
     return res.data.fact;
     }
     catch(e){
+        console.log("Error-",e);
+        return "NO Data Found";
+    }
+    
+}
+async function dogfacts(){
+    try{
+        let res=await axios.get(url2);
+        return res.data.message;
+    }
+    catch(e){
         console.log("error-",e);
-        return "NO data found";
+        return "NO image found in Dog.";
     }
     
 }
